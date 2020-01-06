0e8b9bd98b65034dd42a6ca5658391a425504dde
pawanjayvasnav <pawan.vasnav@highpeaksw.com>
2020-01-06 12:47:12 +0530

    Updates

---
+++ precache-manifest.211324155eb129f3a0170c7a64708b02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "500146f83209fcb7f3a2c0235d83df75",
+    "url": "/monstor-rolodex/index.html"
+  },
+  {
+    "revision": "6ab05e0762fb1214ef59",
+    "url": "/monstor-rolodex/static/css/main.dd9f2b66.chunk.css"
+  },
+  {
+    "revision": "b62b9052603d1f962399",
+    "url": "/monstor-rolodex/static/js/2.382b7515.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monstor-rolodex/static/js/2.382b7515.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6ab05e0762fb1214ef59",
+    "url": "/monstor-rolodex/static/js/main.26a9bafe.chunk.js"
+  },
+  {
+    "revision": "ec4e769dda9237ca9d2e",
+    "url": "/monstor-rolodex/static/js/runtime-main.efc69cdb.js"
+  }
+]);
